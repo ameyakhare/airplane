965059a642540f15ce63e653a9464beb041c4123
Ameya Khare <ameya@airplane.dev>
2022-04-18 13:08:34 -0400

    Created a new task

---
+++ scripts/env_deploy_test.py
@@ -0,0 +1,11 @@
+# Put the main logic of the task in the main function.
+import os
+
+def main(params):
+    print("parameters: ", params)
+
+    # You can return data to show outputs to users.
+    # Outputs documentation: https://docs.airplane.dev/tasks/outputs
+    return [
+        {"environment": os.environ.get("AIRPLANE_ENV"), "message": "Deployed from main"},
+    ]
